dee0b0e31be8b71af7f65d38dc43352de8c21fe3
Ryan McDonald <ryan.mcdonald@outlook.com>
2020-07-10 18:18:19 -0500

    Test Installation

--- scheduler/app/Console/Kernel.php
+++ scheduler/app/Console/Kernel.php
@@ -24,6 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule->exec('fpp -P hourly')->everyFiveMinutes();
     }
 }
