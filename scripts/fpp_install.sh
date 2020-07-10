f9c03688ba95ed02729032de68b190518b0fb091
Ryan McDonald <ryan.mcdonald@outlook.com>
2020-07-10 17:41:05 -0500

    Test Installation

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # fpp-plugin-Template install script
-
-cd ..
+sudo apt-get install sqlite3 php-sqlite3  php-mbstring -y
+cd .. && cd scheduler
+rm -rf vendor
+rm composer.lock
 composer install
-crontab -l | grep -v 'fpp-scheduler-cron'  | crontab -
-(crontab -l ; echo "* * * * * cd /home/fpp/media/plugins/fpp-scheduler-cron && vendor/bin/crunz schedule:run 1>> /dev/null 2>&1") | crontab -
+crontab -l | grep -v 'php artisan schedule:run'  | crontab -
+(crontab -l ; echo "* * * * * cd /path-to-your-project && php artisan schedule:run >> /dev/null 2>&1") | crontab -
 echo crontab updated
--- scripts/fpp_uninstall.sh
+++ scripts/fpp_uninstall.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # fpp-plugin-Template uninstall script
-
-cd ..
+cd .. && cd scheduler
 rm -rf vendor
 echo vendor folder removed
-crontab -l | grep -v '/src/scheduler.php'  | crontab -
+crontab -l | grep -v 'php artisan schedule:run'  | crontab -
 echo crontab removed
