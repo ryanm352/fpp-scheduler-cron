1ff13819387317b4ae4aa84dff486e0454d9583e
Ryan McDonald <ryan.mcdonald@outlook.com>
2020-07-10 17:54:59 -0500

    Test Installation

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # fpp-plugin-Template install script
+sudo apt-get update
 sudo apt-get install sqlite3 php-sqlite3  php-mbstring -y
-cd .. && cd scheduler
-rm -rf vendor
-rm composer.lock
-composer install
 crontab -l | grep -v 'php artisan schedule:run'  | crontab -
 (crontab -l ; echo "* * * * * cd /path-to-your-project && php artisan schedule:run >> /dev/null 2>&1") | crontab -
 echo crontab updated
