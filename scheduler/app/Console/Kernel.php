659a3c6106abf411a0ac820bf236b2c99ad2754e
Ryan McDonald <ryan.mcdonald@outlook.com>
2020-07-10 19:43:33 -0500

    Test Installation

--- scheduler/app/Console/Kernel.php
+++ scheduler/app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
     {
 
         $schedule->call(static function () {
+            echo 'running command!';
             $client = new Client();
             $request = new \GuzzleHttp\Psr7\Request('GET', '/fppxml.php?command=startPlaylist&playList=hourly&repeat=checked&playEntry=1&section=');
             $promise = $client->sendAsync($request)->then(static function ($response) {
