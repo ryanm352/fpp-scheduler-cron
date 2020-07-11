52fc847983c80abb7dc115ca4bfa9e8be669805c
Ryan McDonald <ryan.mcdonald@outlook.com>
2020-07-10 23:26:53 -0500

    Test Installation

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -5,20 +5,20 @@ rm -rf ~/.yarn
 
 echo Installing packages..
 #sudo apt-get update
-#sudo apt-get -y -o Dpkg::Options::=--force-confdef install sqlite3 php-sqlite3 php-mbstring
+sudo apt-get -y -o Dpkg::Options::=--force-confdef install sqlite3 php-sqlite3 nodejs php-mbstring
 
 # install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-export NVM_DIR="~/.nvm"
-source ~/.nvm/nvm.sh
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+#export NVM_DIR="~/.nvm"
+#source ~/.nvm/nvm.sh
 
 # install nvm
-nvm install --lts
-nvm use --lts
+#nvm install --lts
+#nvm use --lts
 
 # install yarn
 curl -o- -L https://yarnpkg.com/install.sh | bash
-exec bash
+source ~/.yarn
 
 # migrate table changes
 echo Running Migrations...
