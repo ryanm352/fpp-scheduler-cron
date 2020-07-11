6975225d1e7e2721fcebc047233dd3a8d4b801b0
Ryan McDonald <ryan.mcdonald@outlook.com>
2020-07-10 22:21:24 -0500

    Test Installation

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 # install php-mbstring
+echo Installing packages..
 sudo apt-get update
-sudo apt-get -y -o Dpkg::Options::=--force-confdef install sqlite3 php-sqlite3 nodejs  php-mbstring
+sudo apt-get -y -o Dpkg::Options::=--force-confdef install sqlite3 php-sqlite3 nodejs yarn  php-mbstring
 
 # migrate table changes
+echo Running Migrations...
 cd ..
 cd scheduler
 touch database/scheduler.db
-php artisan migrate
+php artisan migrate --force
 
 # build front-end
+echo Building UI...
 cd .. && cd scheduler-ui && npm run build
 
+echo Installing Crontab...
 # install crontab
 crontab -l | grep -v 'php artisan schedule:run'  | crontab -
 (crontab -l ; echo "* * * * * cd /home/fpp/media/plugins/fpp-scheduler-cron/scheduler && php artisan schedule:run >> /dev/null 2>&1") | crontab -
-echo crontab updated
+echo ...Done!
