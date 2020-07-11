843a95cef3cca64af801191b863b73a7346083ae
Ryan McDonald <ryan.mcdonald@outlook.com>
2020-07-11 00:45:10 -0500

    Test Installation

---
+++ app.php
@@ -0,0 +1,1 @@
+scheduler/public/index.php
---
+++ scheduler-ui/build/precache-manifest.7c15ee3a764e4880dbbd823637fe42bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5c0184547928c3b7cb8404a2e6660e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d12fd2afb88453ce486b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f86cb28dc384d151b47",
+    "url": "/static/js/2.5572231b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5572231b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12fd2afb88453ce486b",
+    "url": "/static/js/main.699c8d8f.chunk.js"
+  },
+  {
+    "revision": "d035f6f9225d3e3e9e16",
+    "url": "/static/js/runtime-main.620e2968.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
